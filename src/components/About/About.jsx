d3087edb55820a8a8b4753f58fccdc64da0910e3
SSiukh <siukhsasha@gmail.com>
2024-09-01 23:08:03 +0300

    about update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./About.module.css";
 import img from "../../assets/about-img.png";
+import icons from "../../assets/icons.svg";
 
 function About() {
   return (
-    <section className={styles.aboutSection}>
+    <section id="about-section" className={styles.aboutSection}>
       <div className="container">
         <h2 className={styles.aboutTitle}>About</h2>
         <div className={styles.aboutContainer}>
@@ -12,34 +13,47 @@ function About() {
             src={img}
             alt="my about image"
             className={styles.aboutImg}
-            height={375}
+            height={360}
           />
           <ul className={styles.aboutList}>
             <li className={styles.aboutItems}>
               <svg className={styles.aboutIcon}>
-                <use></use>
+                <use href={`${icons}#icon-pointer`}></use>
               </svg>
               <div className={styles.aboutItemsConatiner}>
-                <h3 className={styles.aboutItemsTitle}></h3>
-                <p className={styles.aboutItemsText}></p>
+                <h3 className={styles.aboutItemsTitle}>Frontend Developer</h3>
+                <p className={styles.aboutItemsText}>
+                  I`m actively improving my skills through revent courses,
+                  including the GoIT "Fullstack Developer" course.
+                </p>
               </div>
             </li>
             <li className={styles.aboutItems}>
               <svg className={styles.aboutIcon}>
-                <use></use>
+                <use href={`${icons}#icon-database`}></use>
               </svg>
               <div className={styles.aboutItemsConatiner}>
-                <h3 className={styles.aboutItemsTitle}></h3>
-                <p className={styles.aboutItemsText}></p>
+                <h3 className={styles.aboutItemsTitle}>
+                  Computer Science bachelor
+                </h3>
+                <p className={styles.aboutItemsText}>
+                  I`m studying full-time on a budget basis. I completed
+                  coursework on "Web Application for Time Management" focusing
+                  on the user interface.
+                </p>
               </div>
             </li>
             <li className={styles.aboutItems}>
               <svg className={styles.aboutIcon}>
-                <use></use>
+                <use href={`${icons}#icon-embed2`}></use>
               </svg>
               <div className={styles.aboutItemsConatiner}>
-                <h3 className={styles.aboutItemsTitle}></h3>
-                <p className={styles.aboutItemsText}></p>
+                <h3 className={styles.aboutItemsTitle}>Freelancer</h3>
+                <p className={styles.aboutItemsText}>
+                  I completed several projects on freelance platforms,
+                  particulary related to object-oriented programming and
+                  typesetting.
+                </p>
               </div>
             </li>
           </ul>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { HashLink } from "react-router-hash-link";
 
 function Header() {
   return (
@@ -12,7 +13,9 @@ function Header() {
               <span className={styles.logoSecondSpan}>folio</span>
             </a>
             <ul className={styles.navList}>
-              <li className={styles.navListItem}>About</li>
+              <li className={styles.navListItem}>
+                <HashLink to="../About/About.jsx#about-section">About</HashLink>
+              </li>
               <li className={styles.navListItem}>Experience</li>
               <li className={styles.navListItem}>Projects</li>
               <li className={styles.navListItem}>Contact</li>
